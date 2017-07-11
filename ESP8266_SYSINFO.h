75d5cfb8ed5d2b61d644061ce44e785812e2425a
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-07-11 15:56:52 +0530

    added flash_id, flash_map and system_mac functions

---
+++ ESP8266_SYSINFO.h
@@ -0,0 +1,33 @@
+/*************************************************
+ *
+ * ESP8266 SYSINFO LIBRARY
+ * (UTILITY FUNCTIONS TO PRINT SYSTEM PARAMETERS)
+ *
+ * JULY 11  2017
+ * 
+ * ANKIT BHATNAGAR
+ * ANKIT.BHATNAGARINDIA@GMAIL.COM
+ *************************************************/
+
+#ifndef _ESP8266_SYSINFO_H_
+#define _ESP8266_SYSINFO_H_
+
+#include "osapi.h"
+#include "ets_sys.h"
+#include "os_type.h"
+#include "user_interface.h"
+
+//CUSTOM VARIABLE STRUCTURES/////////////////////////////
+
+//FUNCTION PROTOTYPES/////////////////////////////////////
+//CONFIGURATION FUNCTIONS
+
+//CONTROL FUNCTIONS
+void ICACHE_FLASH_ATTR ESP8266_SYSINFO_PrintFlashChipId(void);
+void ICACHE_FLASH_ATTR ESP8266_SYSINFO_PrintSystemFlashMap(void);
+void ICACHE_FLASH_ATTR ESP8266_SYSINFO_PrintSystemMac(void);
+
+//INTERNAL GPIO FUNCTIONS
+//END FUNCTION PROTOTYPES/////////////////////////////////
+
+#endif
